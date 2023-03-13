0a45745c02b7f9e0de57d095a8db5e1abe9ed562
Jean Claude Ntambara <jc.ntambara@gmail.com>
2023-03-13 17:42:58 -0500

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,178 @@
 #![cfg_attr(not(test), no_std)]
 
-use bare_metal_modulo::{ModNumC, MNum, ModNumIterator};
-use pluggable_interrupt_os::vga_buffer::{BUFFER_WIDTH, BUFFER_HEIGHT, plot, ColorCode, Color, is_drawable};
-use pc_keyboard::{DecodedKey, KeyCode};
-use num::traits::SaturatingAdd;
+use core::panic;
 
-#[derive(Copy,Debug,Clone,Eq,PartialEq)]
-pub struct LetterMover {
-    letters: [char; BUFFER_WIDTH],
-    num_letters: ModNumC<usize, BUFFER_WIDTH>,
-    next_letter: ModNumC<usize, BUFFER_WIDTH>,
-    col: ModNumC<usize, BUFFER_WIDTH>,
-    row: ModNumC<usize, BUFFER_HEIGHT>,
-    dx: ModNumC<usize, BUFFER_WIDTH>,
-    dy: ModNumC<usize, BUFFER_HEIGHT>
+use bare_metal_modulo::{MNum, ModNumC, ModNumIterator};
+use num::traits::SaturatingAdd;
+use pc_keyboard::{DecodedKey, KeyCode};
+use pluggable_interrupt_os::vga_buffer::{
+    is_drawable, plot, Color, ColorCode, BUFFER_HEIGHT, BUFFER_WIDTH,
+};
+
+/* GAME IDEA: SNAKE GAME
+   -- Draw a Snake
+   -- Draw food
+   -- if Snake collides with food, it  grows (increase the snake's length)
+   -- Increase the score from 0 (increment it by 1) and the score is displayed on the screen
+   -- If the Snake hits the wall, the game ends and the user fails.
+   __ If the Snake hits itself, the game ends as well.
+   -- I need to know how to get the snake  and food on the qemu, move the snake, and put food on a different location.
+   -- Press R to restart when the game ends,
+   -- Use the UP, DOWN, LEFT AND RIGHT keys to move the snake.
+   --
+*/
+
+// Constants
+pub const ROWS: usize = 25;
+pub const COLS: usize = 80;
+pub const BLOCK_SIZE: usize = 2;
+pub const MAX_SNAKE_LENGTH: usize = ROWS * COLS / (BLOCK_SIZE * BLOCK_SIZE);
+
+// Colors
+const COLOR_BACKGROUND: Color = Color::Black;
+const COLOR_SNAKE: Color = Color::LightGreen;
+const COLOR_FOOD: Color = Color::Red;
+
+#[derive(Copy, Debug, Clone, Eq, PartialEq)]
+// Directions
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
-impl LetterMover {
-    pub fn new() -> Self {
-        LetterMover {
-            letters: ['A'; BUFFER_WIDTH],
-            num_letters: ModNumC::new(1),
-            next_letter: ModNumC::new(1),
-            col: ModNumC::new(BUFFER_WIDTH / 2),
-            row: ModNumC::new(BUFFER_HEIGHT / 2),
-            dx: ModNumC::new(0),
-            dy: ModNumC::new(0)
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+}
+
+// Game state
+pub struct GameState {
+    pub snake: [(u16, u16); MAX_SNAKE_LENGTH],
+    pub length: usize,
+    pub direction: Direction,
+    pub food: (u16, u16),
+    pub score: usize,
+    // col: ModNumC<usize, BUFFER_WIDTH>,
+    // row: ModNumC<usize, BUFFER_HEIGHT>,
+    // dx: ModNumC<usize, BUFFER_WIDTH>,
+    // dy: ModNumC<usize, BUFFER_HEIGHT>
+}
+
+impl GameState {
+    pub fn new() -> GameState {
+        let mut snake = [(0, 0); MAX_SNAKE_LENGTH];
+        snake[0] = (ROWS as u16 / 2, COLS as u16 / 2);
+
+        GameState {
+            snake,
+            length: 1,
+            direction: Direction::Right,
+            food: (0, 0),
+            score: 0,
         }
     }
 
-    fn letter_columns(&self) -> impl Iterator<Item=usize> {
-        ModNumIterator::new(self.col)
-            .take(self.num_letters.a())
-            .map(|m| m.a())
+    pub fn update_direction(&mut self, direction: Direction) {
+        self.direction = direction;
     }
 
-    pub fn tick(&mut self) {
-        self.clear_current();
-        self.update_location();
-        self.draw_current();
-    }
+    pub fn update(&mut self) {
+        // Move snake
+        let head = self.snake[0];
+        let mut new_head = head;
+        match self.direction {
+            Direction::Up => new_head.0 = new_head.0.wrapping_sub(BLOCK_SIZE as u16),
+            Direction::Down => new_head.0 = new_head.0.wrapping_add(BLOCK_SIZE as u16),
+            Direction::Left => new_head.1 = new_head.1.wrapping_sub(BLOCK_SIZE as u16),
+            Direction::Right => new_head.1 = new_head.1.wrapping_add(BLOCK_SIZE as u16),
+        }
+        self.snake.rotate_right(1);
+        self.snake[0] = new_head;
 
-    fn clear_current(&self) {
-        for x in self.letter_columns() {
-            plot(' ', x, self.row.a(), ColorCode::new(Color::Black, Color::Black));
+        // Check for collision with wall border
+        if new_head.0 >= ROWS as u16 || new_head.1 >= COLS as u16 {
+            panic!("Game Over!");
+        }
+
+        // Check for collision with food, if the snake's head collides with food, the snake's lengh increases and the score increases.
+        if new_head == self.food {
+            self.score += 1;
+            self.length += 1;
+            self.spawn_food();
+        }
+
+        // Check for collision with self, if the snake collide with itself, the game ends
+        for i in 1..self.length {
+            if new_head == self.snake[i] {
+                panic!("Game Over!");
+            }
         }
     }
 
-    fn update_location(&mut self) {
-        self.col += self.dx;
-        self.row += self.dy;
+    // Return true if snake is occupying a given position, else return false.
+    fn is_snake_on_position(&self, position: (u16, u16)) -> bool {
+        for &(row, col) in &self.snake {
+            if row == position.0 && col == position.1 {
+                return true;
+            }
+        }
+        false
     }
 
-    fn draw_current(&self) {
-        for (i, x) in self.letter_columns().enumerate() {
-            plot(self.letters[i], x, self.row.a(), ColorCode::new(Color::Cyan, Color::Black));
+    /* In the spawn_food function we use the current food position as a starting point for generating the new food position.
+    We add 1 to both the row and column of the current position (with wrapping) to get the initial new position.
+    We then enter a loop and keep incrementing the row and column of the new position (again with wrapping)
+    until we find a position that's not occupied by the snake.
+    Once we've found a valid position, we update the food field in the game state and return the new food position.*/
+
+    pub fn spawn_food(&mut self) -> (u16, u16) {
+        let mut new_food = (
+            (self.food.0 + 1) % ROWS as u16,
+            (self.food.1 + 1) % COLS as u16,
+        );
+        while self.is_snake_on_position(new_food) {
+            new_food = (
+                (new_food.0 + 1) % ROWS as u16,
+                (new_food.1 + 1) % COLS as u16,
+            );
+        }
+        self.food = new_food;
+        new_food
+    }
+
+    pub fn draw_snake(&self, buffer: &mut [u8]) {
+        for &(row, col) in self.snake.iter().take(self.length) {
+            plot(
+                '*',
+                col as usize,
+                row as usize,
+                ColorCode::new(Color::LightCyan, Color::Black),
+            );
+            /*
+            let start_idx = ((row as usize) * COLS + (col as usize)) * BLOCK_SIZE;
+            for i in 0..BLOCK_SIZE {
+                for j in 0..BLOCK_SIZE {
+                    let idx = start_idx + (i * COLS + j) * 4;
+                    buffer[idx..idx+4].copy_from_slice(&[0xff, 0xff, 0xff, 0xff]);
+                }
+            }
+            */
         }
     }
 
+    // In this function, I want to let the user type 'R' to restart the game when the game is over.
     pub fn key(&mut self, key: DecodedKey) {
         match key {
-            DecodedKey::RawKey(code) => self.handle_raw(code),
-            DecodedKey::Unicode(c) => self.handle_unicode(c)
-        }
-    }
-
-    fn handle_raw(&mut self, key: KeyCode) {
-        match key {
-            KeyCode::ArrowLeft => {
-                self.dx -= 1;
-            }
-            KeyCode::ArrowRight => {
-                self.dx += 1;
-            }
-            KeyCode::ArrowUp => {
-                self.dy -= 1;
-            }
-            KeyCode::ArrowDown => {
-                self.dy += 1;
-            }
+            DecodedKey::RawKey(KeyCode::R) | DecodedKey::Unicode('r') => {}
             _ => {}
-        }
-    }
-
-    fn handle_unicode(&mut self, key: char) {
-        if is_drawable(key) {
-            self.letters[self.next_letter.a()] = key;
-            self.next_letter += 1;
-            self.num_letters = self.num_letters.saturating_add(&ModNumC::new(1));
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,80 @@
 #![no_std]
 #![no_main]
 
-use pc_keyboard::DecodedKey;
-use pluggable_interrupt_os::HandlerTable;
-use pluggable_interrupt_os::vga_buffer::clear_screen;
-use pluggable_interrupt_template::LetterMover;
 use crossbeam::atomic::AtomicCell;
+use pc_keyboard::DecodedKey;
+use pluggable_interrupt_os::vga_buffer::clear_screen;
+use pluggable_interrupt_os::HandlerTable;
+use snake_game_rust::{GameState, BLOCK_SIZE, COLS, ROWS};
 
 #[no_mangle]
+#[allow(non_snake_case)]
 pub extern "C" fn _start() -> ! {
-    HandlerTable::new()
-        .keyboard(key)
-        .timer(tick)
-        .startup(startup)
-        .cpu_loop(cpu_loop)
-        .start()
-}
+    static LAST_KEY: AtomicCell<Option<DecodedKey>> = AtomicCell::new(None);
+    const SCREEN_SIZE: usize = ROWS * COLS * BLOCK_SIZE * BLOCK_SIZE * 4;
 
-static LAST_KEY: AtomicCell<Option<DecodedKey>> = AtomicCell::new(None);
-static TICKS: AtomicCell<usize> = AtomicCell::new(0);
+    let mut kernel = GameState::new();
 
-fn cpu_loop() -> ! {
-    let mut kernel = LetterMover::new();
-    let mut last_tick = 0;
+    // Spawn the initial food
+    kernel.spawn_food();
+
+    // Draw game screen
+    let buffer = unsafe { core::slice::from_raw_parts_mut(0xb8000 as *mut u8, SCREEN_SIZE) };
+    for byte in buffer.iter_mut() {
+        *byte = 0x00;
+    }
     loop {
         if let Some(key) = LAST_KEY.load() {
             LAST_KEY.store(None);
             kernel.key(key);
         }
-        let current_tick = TICKS.load();
-        if current_tick > last_tick {
-            last_tick = current_tick;
-            kernel.tick();
+
+        // Clear the screen buffer
+        // buffer.iter_mut().for_each(|b| *b = 0);
+
+        // Draw the food
+        let (food_row, food_col) = kernel.food;
+        let food_start_idx =
+            ((food_row as usize) * COLS + (food_col as usize)) * BLOCK_SIZE * BLOCK_SIZE * 4;
+        for i in 0..BLOCK_SIZE {
+            for j in 0..BLOCK_SIZE {
+                let idx = food_start_idx + (i * COLS + j) * 4;
+                buffer[idx..idx + 4].copy_from_slice(&[0xff, 0x00, 0x00, 0xff]);
+            }
         }
+
+        kernel.draw_snake(buffer);
+
+        // ... keyboard input handling ...
     }
-}
-
-fn tick() {
-    TICKS.fetch_add(1);
-}
-
-fn key(key: DecodedKey) {
-    LAST_KEY.store(Some(key));
 }
 
 fn startup() {
     clear_screen();
 }
+
+/*
+fn render_buffer(buffer: &[u8]) {
+    let width = 80;
+    let height = 25;
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for y in 0..height {
+        for x in 0..width {
+            let index = (y * width + x) as usize;
+            let char_code = buffer[index];
+            let color = buffer[index + 1];
+
+            // We need to convert the color from our 256-color palette to a VGA color, which uses 4 bits for the background color and 4 bits for the foreground color
+            let vga_color = ((color >> 4) << 4) | (color & 0x0f);
+
+            // Write the character and color to the VGA buffer
+            unsafe {
+                *vga_buffer.offset(index as isize * 2) = char_code;
+                *vga_buffer.offset(index as isize * 2 + 1) = vga_color;
+            }
+        }
+    }
+}
+*/
